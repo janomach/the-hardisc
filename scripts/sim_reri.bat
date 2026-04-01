c16f4c3ada8eafab65869d5bef114323770085c0
Michal Holienčík <zs.michal.holiencik@gmail.com>
2026-04-01 19:47:55 +0200

    Outsource AHB requests to ahb_controller

--- scripts/sim_reri.bat
+++ scripts/sim_reri.bat
@@ -10,7 +10,7 @@ rem -------------------------------------------------------------------------
 
 setlocal
 
-set SRC=rtl/edac.sv rtl/ecc_monitor.sv rtl/reri_error_bank.sv ver/tb_reri.sv
+set SRC=rtl/edac.sv rtl/ecc_monitor.sv peripherals/ahb_controller.sv peripherals/reri_error_bank.sv ver/tb_reri.sv
 
 echo Compiling...
 iverilog -g2012 -I rtl -o sim_reri.vvp %SRC%
